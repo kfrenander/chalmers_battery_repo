3752ae98370a6ffb4381f094e6774952b0300040
kfrenand <kristian.frenander@volvocars.com>
2024-02-27 14:00:31 +0100

    Fixed so that local time handling in python and matlab is correctly interpreted in data reader.

--- test_data_analysis/read_mux_file_to_df.py
+++ test_data_analysis/read_mux_file_to_df.py
@@ -2,21 +2,32 @@ from datetime import datetime
 import pandas as pd
 
 
-def unix_time_to_local_datetime(unix_time):
+def utc_unix_time_to_local_datetime(unix_time):
     return datetime.utcfromtimestamp(unix_time / 1e3)
 
 
-def convert_unix_time_columns_to_local_datetime(df, unix_time_columns):
+def local_unix_time_to_local_datetime(unix_time):
+    return datetime.fromtimestamp(unix_time / 1e3)
+
+
+def convert_unix_time_columns_to_local_datetime(df, unix_time_columns, local_time):
     if isinstance(unix_time_columns, str):  # Check if a single column is provided
         unix_time_columns = [unix_time_columns]  # Convert single column to list
     for col in unix_time_columns:
-        df[col + '_Local_DateTime'] = df[col].apply(unix_time_to_local_datetime)
+        if local_time:
+            df[col + '_Local_DateTime'] = df[col].apply(local_unix_time_to_local_datetime)
+        else:
+            df[col + '_Local_DateTime'] = df[col].apply(utc_unix_time_to_local_datetime)
     return df
 
 
 def convert_mux_log_to_df(mux_log):
     df = pd.read_csv(mux_log, names=['time1', 'voltage1', 'time2', 'voltage2'])
-    df = convert_unix_time_columns_to_local_datetime(df, ['time1', 'time2'])
+    if mux_log.endswith('.csv'):
+        local_time=True
+    else:
+        local_time=False
+    df = convert_unix_time_columns_to_local_datetime(df, ['time1', 'time2'], local_time=local_time)
     for col in df.columns:
         if 'voltage' in col:
             df[col] = filter_sudden_jumps(df[col], 0.5)
@@ -35,5 +46,8 @@ def filter_sudden_jumps(series, threshold):
 
 if __name__ == '__main__':
     mux_log = r"\\sol.ita.chalmers.se\groups\eom-et-alla\Research\HaliBatt\SiGr_materials\CtrlMsmt\2024     2    14    11    22    29"
+    csv_log = r"\\sol.ita.chalmers.se\groups\eom-et-alla\Research\HaliBatt\SiGr_materials\ICI_msmt\voltage_data_2024-02-27_12_07_18_984.csv"
     df = convert_mux_log_to_df(mux_log)
     df.filter(like='voltage').plot()
+    df_python = convert_mux_log_to_df(csv_log)
+    df_python.filter(like='voltage').plot()
