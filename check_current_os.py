4541081902968cdc994d09994d4fc7fb5690ebff
kfrenand <kristian.frenander@volvocars.com>
2024-06-10 21:44:35 +0200

    Fixed functionality on Windows.

--- check_current_os.py
+++ check_current_os.py
@@ -4,38 +4,51 @@ from pathlib import Path, PureWindowsPath, PurePosixPath
 
 def set_correct_path_type(input_path):
     current_os = platform.system()
+    update_need = 0
     if current_os == 'Windows' and len(Path(input_path).parts) > 1:
-        print('Windows path provided, return PureWindowsPath')
-        return PureWindowsPath(input_path)
+        if Path(input_path).parts[1] == 'mnt':
+            print('Posix path provided on Windows, return PurePosixPath, update needed')
+            update_need = 1
+            return PurePosixPath(input_path), update_need
+        else:
+            print('Windows path provided on Windows, no update needed')
+            update_need = 0
+            return PureWindowsPath(input_path), update_need
     elif current_os == 'Windows' and len(Path(input_path).parts) == 1:
-        print('Posix path provided, return PurePosixPath')
-        return PurePosixPath(input_path)
+        print('Posix path provided on Windows, return PurePosixPath, update needed')
+        update_need = 1
+        return PurePosixPath(input_path), update_need
     elif current_os == 'Linux' and len(Path(input_path).parts) > 1:
-        print('Posix path provided, return PurePosixPath')
-        return PurePosixPath(input_path)
+        print('Posix path provided on Linux, no update needed')
+        update_need = 0
+        return PurePosixPath(input_path), update_need
     elif current_os == 'Linux' and len(Path(input_path).parts) == 1:
-        print('Windows path provided, return PureWindowsPath')
-        return PureWindowsPath(input_path)
+        print('Windows path provided on Linux, return PureWindowsPath and update')
+        update_need = 1
+        return PureWindowsPath(input_path), 1
 
 
 # Function to get the correct path
 def get_correct_path(input_path):
     # Define the base paths for Windows and Linux
-    windows_base_path = Path(r'\\sol.ita.chalmers.se\groups\batt_lab_data')
-    linux_base_path = Path('/mnt/batt_lab_data')
+    windows_base_path = PureWindowsPath(r'\\sol.ita.chalmers.se\groups\batt_lab_data')
+    linux_base_path = PurePosixPath('/mnt/batt_lab_data')
 
     # Determine the operating system
     current_os = platform.system()
-    input_path = set_correct_path_type(input_path)
+    input_path, update_bool = set_correct_path_type(input_path)
 
     # If the path is absolute and in Windows format
-    if input_path.is_absolute() and current_os == 'Linux' and input_path.drive == '\\\\sol.ita.chalmers.se\\groups':
+    if input_path.is_absolute() and current_os == 'Linux' and update_bool:
         # Convert the Windows absolute path to Linux absolute path
         relative_path = input_path.relative_to(windows_base_path)
         correct_path = linux_base_path / relative_path
-    elif current_os == 'Windows':
-        correct_path = windows_base_path / input_path.relative_to(linux_base_path)
+    elif input_path.is_absolute() and current_os == 'Windows' and update_bool:
+        # Convert the Linux absolute path to Windows absolute path
+        relative_path = input_path.relative_to(linux_base_path)
+        correct_path = windows_base_path / relative_path
     else:
+        # Otherwise let path through
         correct_path = input_path
 
     return correct_path
