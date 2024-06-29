23b5d1203a7c7f9bad4c77570a100deef4275283
jutsell <jutsell@chalmers.se>
2024-06-29 17:38:15 +0200

    small fixes

--- temperature_monitor/collector/collector.c
+++ temperature_monitor/collector/collector.c
@@ -3,77 +3,72 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <errno.h>
 
+#define PORT 10001
 #define BUFFER_SIZE 1024
 
-void log_message(const char *filename, const char *message){
-    FILE *logfile = fopen(filename, "a"); //will create the file if it does not exist
-    if (logfile == NULL){
-        printf("Failed to open log file");
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usaage: %s <server_address> <output_file>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    fprintf(logfile, "%s\n", message);
-    printf("Temp: %s\n", message);
-    fflush(logfile);
-    fclose(logfile);
-}
 
-int main(int argc, char *argv[]){
-    printf("working?\n");
-    fflush(stdout);
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <IP> <PORT> <LOGFILE>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    printf("working2\n");
-    const char *ip = argv[1];
-    int port = atoi(argv[2]);
-    const char *logfile = argv[3];
-
+    char *server_address = argv[1];
+    char *output_file = argv[2];
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
+    FILE *file;
 
-    // Create socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
-    // Set up the server address structure
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
+    serv_addr.sin_port = htons(PORT);
 
-    // Convert IP address from text to binary form
-    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
-        printf("Invalid address/Address is not supported\n");
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, server_address, &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address or address not supported");
+        close(sock);
         exit(EXIT_FAILURE);
     }
 
     // Connect to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Connection failed\n");
+        printf("Connection failed");
+        close(sock);
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected to the server");
+    printf("Connected to %s on port %d\n", server_address, PORT);
 
-    // Receive data from the server
-    int read_size;
-    while ((read_size = read(sock, buffer, BUFFER_SIZE - 1)) > 0) {
-        buffer[read_size] = '\0';
-        log_message(logfile, buffer);
+    // Open file for appending
+    file = fopen(output_file, "a");
+    if (file == NULL) {
+        printf("Failed to open file");
+        close(sock);
+        exit(EXIT_FAILURE);
     }
 
-    if (read_size == 0) {
-        printf("Server closed the connection");
-    } else if (read_size < 0) {
+    // Listen for data from the server
+    int read_size;
+    while ((read_size = read(sock, buffer, BUFFER_SIZE - 1)) > 0) {
+        buffer[read_size] = '\0';  // Null-terminate the buffer
+        printf("Received: %s\n", buffer);
+        fprintf(file, "%s", buffer);
+        fflush(file);  // Ensure the data is written to the file immediately
+    }
+
+    if (read_size < 0) {
         printf("Read error");
     }
 
+    // Close the file and the socket
+    fclose(file);
     close(sock);
+
     return 0;
 }
-
