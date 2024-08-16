884c66839cbacba88079f278ed36aadb023c668d
kfrenand <kristian.frenander@volvocars.com>
2024-08-16 12:25:52 +0200

    New script for entropy measurements.

--- pyvisa_interface/entropy_over_pyvisa_3458.py
+++ pyvisa_interface/entropy_over_pyvisa_3458.py
@@ -1,5 +1,6 @@
 import pyvisa
 import time
+import numpy as np
 
 
 rm = pyvisa.ResourceManager()
@@ -15,7 +16,28 @@ obj25 = rm.open_resource(instr25)
 
 obj21.write('END ALWAYS')
 obj22.write('END ALWAYS')
+obj21.write('DCI 1')
 
-meas_curren = obj21.query('DCI?')
-meas_voltage = obj22.query('DCV?')
-print(f'\rLatest measurement: {meas_curren} and {meas_voltage}')
+start_time = time.time()
+
+while time.time < start_time + 10:
+    meas_timestamp = time.time()
+    try:
+        meas_current = float(obj21.query('DCI?'))
+    except:
+        print(f'Current measurement failed at {meas_timestamp}')
+        meas_current = np.nan
+
+    try:
+        meas_voltage = float(obj22.query('DCV?'))
+    except:
+        print(f'Voltage measurement failed at {meas_timestamp}')
+        meas_voltage = np.nan
+
+    try:
+        meas_fres = float(obj25.query('MEAS:FRES?'))
+    except:
+        print(f'Resistance measurement failed at {meas_timestamp}')
+        meas_fres = np.nan
+
+    print(f'\rLatest measurement: {meas_current:.2e} and {meas_voltage:.2e}')
