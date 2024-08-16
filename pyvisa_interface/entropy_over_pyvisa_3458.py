12d8fe58e3c7595314ea8a8bf54019edf5422375
kfrenand <kristian.frenander@volvocars.com>
2024-08-16 12:16:04 +0200

    New script for entropy measurements.

---
+++ pyvisa_interface/entropy_over_pyvisa_3458.py
@@ -0,0 +1,17 @@
+import pyvisa
+import time
+
+
+rm = pyvisa.ResourceManager()
+devices = rm.list_resources()
+
+instr21 = 'GPIB0::21::INSTR'
+instr22 = 'GPIB0::22::INSTR'
+instr25 = 'GPIB0::25::INSTR'
+
+obj21 = rm.open_resource(instr21)
+obj22 = rm.open_resource(instr22)
+obj25 = rm.open_resource(instr25)
+
+obj21.write('END ALWAYS')
+obj22.write('END ALWAYS')
