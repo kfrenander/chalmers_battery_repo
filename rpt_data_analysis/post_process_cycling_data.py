b74efe3239a7d72881934cd7b5b916c52b9a03e3
krifren <krifren@chalmers.se>
2025-02-10 11:15:54 +0100

    Updates to include ICI analysis when applicable, and using extrapolation to find fce at given q.

--- rpt_data_analysis/post_process_cycling_data.py
+++ rpt_data_analysis/post_process_cycling_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from scipy.integrate import trapz
 from misc_classes.test_metadata_reader import MetadataReader
+from scipy.optimize import curve_fit, root_scalar
+from test_data_analysis.ici_analysis_class import ICIAnalysis
 
 
 def initiate_test_names_pulse_charge():
@@ -48,6 +50,7 @@ class CycleAgeingDataIndexer:
         self.test_naming = None
         self.ageing_data = None
         self.combined_data = None
+        self.combined_data_arbitrary_replicates = None
         self.visual_profile = VisualProfileAllAgeingTests()
 
     def index_data(self):
@@ -113,12 +116,46 @@ class CycleAgeingDataIndexer:
                 self.combined_data[tn] = combined_df
         return None
 
+    def generate_arbitrary_replicates_combined_data(self):
+        test_meta_data = MetadataReader()
+        TEST_NAMES = test_meta_data.excel_data.TEST_CONDITION.unique()
+        self.combined_data_arbitrary_replicates = {}
+        for tn in TEST_NAMES:
+            tmp_list = []
+            for ch_id, ag_data in self.ageing_data.items():
+                if ag_data.meta_data.test_condition == tn:
+                    tmp_list.append(ag_data.rpt_data)
+            if tmp_list:
+                suffix_list = [f"_cell{i + 1}" for i in range(len(tmp_list))]
+
+                # Start with the first dataframe in the list
+                combined_df = tmp_list[0]
+
+                # Merge the rest of the dataframes
+                for i in range(1, len(tmp_list)):
+                    combined_df = pd.merge_asof(
+                        combined_df,
+                        tmp_list[i],
+                        left_on='fce',
+                        right_on='fce',
+                        suffixes=('', suffix_list[i])
+                    )
+                filtered_columns = [
+                    col for col in combined_df.columns
+                    if 'cap_relative' in col and 'sigma_cap_relative' not in col
+                ]
+                combined_df['avg_rel_cap'] = combined_df[filtered_columns].mean(axis=1)
+                combined_df['std_rel_cap'] = combined_df[filtered_columns].std(axis=1)
+                self.combined_data_arbitrary_replicates[tn] = combined_df
+        return None
+
     def run(self, top_directory, project_key='pulse_charging'):
         self.top_dir = top_directory
         self.test_naming = initiate_test_names_pulse_charge()
         self.index_data()
         self.fill_ageing_data()
         self.generate_replicate_combined_data()
+        self.generate_arbitrary_replicates_combined_data()
 
 
 class CycleAgeingDataReader:
@@ -129,12 +166,17 @@ class CycleAgeingDataReader:
         self.pkl_files = list_of_files
         self.rpt_data = None
         self.ica_data = None
+        self.ici_data = None
         self.dyn_data = None
         self.meta_data = None
         self.average_temperature = None
+        self.popt = None
+        self.pcov = None
         self.read_rpt_summary()
         self.read_ica_data()
+        self.read_ici_data()
         self.read_metadata()
+        self.popt, self.pcov = self.fit_degradation_function()
         self.TEST_NAME = self.meta_data.test_condition
         self.visual_profile = VisualProfileUniqueTest(test_name=self.TEST_NAME)
 
@@ -144,6 +186,11 @@ class CycleAgeingDataReader:
             if 'rpt_summary' in pkl_file:
                 try:
                     self.rpt_data = pd.read_pickle(pkl_file)
+                    if not self.rpt_data['date'].is_monotonic_increasing:
+                        print(f'!!! FOUND NON-MONOTONIC TIME SERIES IN {self.chnl_id} FOR TEST {self.TEST_NAME}!!!')
+                        self.rpt_data = self.rpt_data.sort_values(by='date').reset_index(drop=True)
+                        rpt_idx_update = [f'rpt_{i}' for i in range(1, self.rpt_data.shape[0] + 1)]
+                        self.rpt_data = self.rpt_data.set_axis(rpt_idx_update)
                     self.rpt_data['fce'] = np.arange(0, self.rpt_data.shape[0]*40, 40)
 
                     found_summary = True
@@ -163,6 +210,21 @@ class CycleAgeingDataReader:
                     print(f"An error occurred while reading file '{pkl_file}': {e}")
         if not found_summary:
             print(f'No rpt summary file found for data from channel {self.chnl_id}.\nPlease check data.')
+
+    def fit_degradation_function(self):
+        """
+            Fit the Q(fce) function to data.
+            Args:
+               fce (array-like): FCE data
+               q (array-like): Capacity data
+            Returns:
+               popt (tuple): Optimized parameters (q0, tau, beta)
+               pcov (2D array): Covariance matrix of the fit
+        """
+        x_data = self.rpt_data.fce.astype('float')
+        y_data = self.rpt_data.cap_relative.astype('float')
+        popt, pcov = curve_fit(self._q_function, x_data, y_data, p0=[1, 1000, 1])
+        return popt, pcov
 
     def read_metadata(self):
         for file in self.pkl_files:
@@ -197,6 +259,33 @@ class CycleAgeingDataReader:
         if not found_ica:
             print(f'No ica files found for data from channel {self.chnl_id}.\nPlease check data.')
 
+    def read_ici_data(self):
+        self.ici_data = {}
+        found_ici = False
+        for pkl_file in self.pkl_files:
+            if 'ici_dump' in pkl_file:
+                found_ici = True
+                rpt_idx = ''
+                try:
+                    rpt_idx = extract_rpt_from_filepath(pkl_file)
+                except re.error:
+                    print('Not able to resolve regex for identifying RPT index')
+                except (TypeError, AttributeError, SyntaxError, ValueError, UnicodeError, IndexError) as e:
+                    print(f'An error occurred when trying to find RPT index for {pkl_file}: {e}')
+                except Exception as e:
+                    print(f'Unexpected error occurred when trying to find RPT index for {pkl_file}: {e}')
+                if rpt_idx:
+                    try:
+                        df = pd.read_pickle(pkl_file)
+                        self.ici_data[rpt_idx] = ICIAnalysis(df)
+                        tmp_output = self.ici_data[rpt_idx].perform_ici_analysis()
+                    except (PermissionError, IOError, EOFError, ValueError, TypeError, MemoryError) as e:
+                        print(f"An error occurred while reading file '{pkl_file}': {e}")
+                    except Exception as e:
+                        print(f"An error occurred while reading file '{pkl_file}': {e}")
+        if not found_ici:
+            print(f'No ici files found for data from channel {self.chnl_id}.\nPlease check data.')
+
     def read_dynamic_data(self):
         self.dyn_data = {}
         found_dynamic_data = False
@@ -218,6 +307,32 @@ class CycleAgeingDataReader:
         self.average_temperature = (trapz(self.dyn_data.temperature, self.dyn_data.float_time)
                                     / self.dyn_data.float_time.max())
         # TODO: Implement some kind of histogram for temperature visualisation
+
+    def find_fce_at_given_q(self, q_target):
+        """
+        Find the time at which the fitted Q(t) function yields a given Q.
+        Args:
+            q_target (float): Target capacity
+        Returns:
+            fce (float): Cycles corresponding to the target Q
+        """
+        if self.popt is None:
+            raise ValueError("You must fit the function before calling this method.")
+
+        # Solve for t such that Q(fce) = q_target
+        def equation(fce):
+            return q_target - self._q_function(fce, *self.popt)
+
+        result = root_scalar(equation, bracket=[1e-5, 1e5], method='brentq')
+        if result.converged:
+            return result.root
+        else:
+            raise RuntimeError("Failed to converge to a solution.")
+
+    def _q_function(self, fce, q0, tau, beta):
+        """The function to fit: Q(fce) = q0 * exp(-(fce/tau)^beta)"""
+        fce = np.asarray(fce)
+        return q0 * np.exp(-(fce / tau) ** beta)
 
 
 class VisualProfileAllAgeingTests:
